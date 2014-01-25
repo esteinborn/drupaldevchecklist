c08b72b8fb300ff1937398adfb6c220afda2f4d2
Tom Atkins <treat@mac.com>
2014-01-25 01:06:06 -0500

    good Nightgit add .

--- gruntfile.js
+++ gruntfile.js
@@ -1,9 +1,10 @@
 module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-compass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	
+
 	grunt.initConfig({
+		
 		uglify:  {
 			my_target: {
 				files: {
@@ -12,13 +13,28 @@ module.exports = function (grunt) {
 			}// my target
 		}, //uglify
 		
+		compass: {
+			dev: {
+				options: {
+					config: 'config.rb'
+					
+				}//options
+			}//dev
+		},//compass
+		
+				
 		watch: {
 			options: { livereload: true },
 			scripts: {
 				files: ['_/components/js/*.js' ],
 			    tasks: ['uglify']
-				
 			}, //scripts
+			
+				sass: {
+				files: ['_/components/sass/*.scss'],
+				tasks: ['compass:dev']
+			}, //sass
+			
 			html: {
 				files: ['*.html']
 			}

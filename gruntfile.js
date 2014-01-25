0bc021d4baa358fa64c75a6c219edda66bc874e8
Tom Atkins <treat@mac.com>
2014-01-25 00:03:10 -0500

    Successful Grunt File created

---
+++ gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = function (grunt) {
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.initConfig({
+		uglify:  {
+			my_target: {
+				files: {
+					'_/js/script.js' :['_/components/js/*.js']
+				}//files
+			}// my target
+		} //uglify
+	})//init config
+
+} //exports
